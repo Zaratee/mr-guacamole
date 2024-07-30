609b48e421669c487931d13cb36b7a552c292a01
Zaratee <39096560+Zaratee@users.noreply.github.com>
2024-07-30 17:55:55 -0600

    wip navbar

--- src/layouts/Landing/components/Navbar/Navbar.js
+++ src/layouts/Landing/components/Navbar/Navbar.js
@@ -4,12 +4,14 @@ import logo from 'src/assets/common/logo.png'
 export const Navbar = () => {
   return (
     <div className='p-6 px-14 flex'>
-      <MenuIcon size='2.5rem'/>
+      <div className='cursor-pointer select-none'>
+        <MenuIcon size='2.5rem'/>
+      </div>
       <div className=' flex gap-14 ml-14 items-center  w-[70%]'>
-        <div>Inicio</div>
-        <div>Nosotros</div>
-        <div>Catálogo</div>
-        <div>Contactanos</div>
+        <div className='cursor-pointer select-none'>Inicio</div>
+        <div className='cursor-pointer select-none'>Nosotros</div>
+        <div className='cursor-pointer select-none'>Catálogo</div>
+        <div className='cursor-pointer select-none'>Contactanos</div>
       </div>
       <img className='hidde-logo w-[165px] h-[50px] fixed right-14 top-6' src={logo}/>
     </div>
--- src/pages/Inicio/Inicio.js
+++ src/pages/Inicio/Inicio.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import inicioLogo from 'src/assets/common/inicioLogo.png'
+import anime from 'animejs/lib/anime.es.js';
+
+
 export const Inicio = () => {
+  useEffect(() => {
+    anime({
+      targets: '.filter',
+      keyframes: [
+        {translateX: 200},
+        {translateX: -128},
+      ],
+      easing: 'easeOutQuad',
+    
+    });
+  })
   return (
     <div className=' h-full flex items-center justify-center md:justify-around   px-4'>
-      <div className='flex mt-16 flex-col gap-3  w-[100%]  md:w-fit'>
-        <div className=' text-6xl font-bold lexend'>Catálogo Virtual</div>
+      <div className='flex flex-col gap-3  w-[100%]  md:w-fit'>
+        <div className='filter text-6xl font-bold lexend'>Catálogo Virtual</div>
         <div className=' text-4xl font-extralight lexend'> Cotiza los productos para<br/> recibirlos a domicilio! </div>
-        <div className=' cursor-pointer mt-3 lexend bg-gradient-to-r from-[#4C8F47] to-[#C2D856] text-xl rounded-full w-fit px-10 py-3 font-bold text-white'> Ver Catálogo </div>
+        <div className=' select-none cursor-pointer mt-3 lexend bg-gradient-to-r from-[#4C8F47] to-[#C2D856] text-xl rounded-full w-fit px-10 py-3 font-bold text-white'> Ver Catálogo </div>
       </div>
       <img className=' mt-16 collapse md:visible w-0 md:w-[450px] h-[450px]' src={inicioLogo}/>
     </div>
