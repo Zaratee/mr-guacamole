30448bc41493294220708db0604188438d2bb1c3
Zaratee <39096560+Zaratee@users.noreply.github.com>
2024-08-01 15:22:24 -0600

    Navbar wip
    Added AnimeJs

---
+++ src/assets/svg/itemBorderNavbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ItemBorderNavbar = ({width, height}) => {
+  return (
+    <svg id='border1' width="31" height="14" viewBox="0 0 31 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="0.25" y="0.25" width="30.5" height="13.5" rx="1.75" stroke="black" stroke-width="0.5"/>
+    </svg>
+
+  )
+}
+
+export default ItemBorderNavbar
--- src/layouts/Landing/components/Navbar/Navbar.js
+++ src/layouts/Landing/components/Navbar/Navbar.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MenuIcon from 'src/assets/icones/menu'
 import logo from 'src/assets/common/logo.png'
+import anime from 'animejs/lib/anime.es.js';
+import ItemBorderNavbar from 'src/assets/svg/itemBorderNavbar';
+
 export const Navbar = () => {
+
+  useEffect(() => {
+    anime({
+      targets: '#border1 path',
+      strokeDashoffset: [anime.setDashoffset, 0],
+      easing: 'easeOutQuad',
+      duration: 2000,
+      delay: function(el, i) { return i * 450 },
+      direction: 'alternate',
+    });
+  })
   return (
     <div className='p-6 px-14 flex'>
       <div className='cursor-pointer select-none'>
         <MenuIcon size='2.5rem'/>
       </div>
       <div className=' flex gap-14 ml-14 items-center  w-[70%]'>
-        <div className='cursor-pointer select-none'>Inicio</div>
-        <div className='cursor-pointer select-none'>Nosotros</div>
-        <div className='cursor-pointer select-none'>Catálogo</div>
-        <div className='cursor-pointer select-none'>Contactanos</div>
+        <div className='cursor-pointer select-none bg'>
+          Inicio
+          <div><ItemBorderNavbar width={38}/></div>
+        </div>
+        <div className='cursor-pointer select-none'>
+          Nosotros
+          <div><ItemBorderNavbar width={68}/></div>
+        </div>
+        <div className='cursor-pointer select-none'>
+          Catálogo
+        <div><ItemBorderNavbar width={66}/></div>
+        </div>
+        <div className='cursor-pointer select-none'>
+          Contactanos
+          <div><ItemBorderNavbar width={94}/></div>
+        </div>
       </div>
       <img className='hidde-logo w-[165px] h-[50px] fixed right-14 top-6' src={logo}/>
     </div>
