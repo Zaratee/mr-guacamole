341a4cc484a476377781a5816c16a8d703673941
Zaratee <39096560+Zaratee@users.noreply.github.com>
2024-08-08 14:46:24 -0600

    Tienda page
    Responsive wip

--- src/layouts/Nosotros/NosotrosLayout.js
+++ src/layouts/Nosotros/NosotrosLayout.js
@@ -6,9 +6,8 @@ export const NosotrosLayout = () => {
     const navigate = useNavigate()
   return (
     <div className='w-[100vw] h-[100vh] relative bg-slate-100'>
-        NosotrosLayout
         <Outlet/>
-        <div onClick={()=>{navigate('/inicio')}} className=' cursor-pointer absolute right-2 bottom-2 rounded-full bg-[#C2D856] w-fit p-3 shadow-lg'>
+        <div onClick={()=>{navigate('/inicio')}} className=' cursor-pointer absolute left-2 top-2 rounded-full bg-[#C2D856] w-fit p-3 shadow-lg'>
             <BackArrowIcon/>
         </div>
     </div>
